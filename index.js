2426f18127be0b4f9f6f76f5e287d59cdd72db40
Milan Mazik <mazik1990@hotmail.com>
2024-08-11 17:11:42 +0200

    separating modules

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = MONGODB_URI;
-
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-  blog.save().then((result) => {
-    res.status(201).json(result);
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
