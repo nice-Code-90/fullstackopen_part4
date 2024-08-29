be5fa7cf08eca232c83d5df6a18cd27a636f4938
Milan Mazik <mazik1990@hotmail.com>
2024-08-29 17:22:56 +0200

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,30 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 
   res.json(blogs);
 });
 
 blogsRouter.post("/", async (req, res) => {
   const body = req.body;
+  const user = await User.findOne();
+
   if (body.url && body.title) {
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes ? body.likes : 0,
+      user: user._id,
     });
 
     const savedBlog = await blog.save();
+
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     res.status(201).json(savedBlog);
   } else {
     res.status(400).end();
--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,11 @@ usersRouter.post("/", async (req, res) => {
 });
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    url: 1,
+    title: 1,
+    author: 1,
+  });
   response.json(users);
 });
 module.exports = usersRouter;
